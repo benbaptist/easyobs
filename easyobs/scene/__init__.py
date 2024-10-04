889a42d2732151f646c85da4a17a95d7b411ed15
Ben Baptist <sawham6@mgail.com>
2024-10-04 00:55:50 -0400

    custom screenshots

--- easyobs/scene/__init__.py
+++ easyobs/scene/__init__.py
@@ -11,16 +11,30 @@ class Scene:
         return self.name
     
     def __repr__(self):
-        return f"Scene(name={self.name}, uuid={self.uuid})"
+        return f"<Scene(name={self.name}, uuid={self.uuid})>"
 
     @property
     def screenshot(self):
+        """
+        Get a screenshot of the scene.
+        """
+
+        return self.get_screenshot(
+            self.root.video_settings.output_width, 
+            self.root.video_settings.output_height
+        )
+
+    def get_screenshot(self, width, height, quality=92):
+        """
+        Get a screenshot of the scene.
+        """
+
         resp = self.root.client.get_source_screenshot(
             self.name, 
             "jpg", 
-            self.root.video_settings.output_width, 
-            self.root.video_settings.output_height, 
-            95
+            width, 
+            height, 
+            quality
         )
 
         parsed = datauri.parse(resp.image_data)
